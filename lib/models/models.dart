c40f93c5a731c4c0ed5efd20f81ea71223f2d055
Levente <slevi1001@gmail.com>
2023-04-04 22:36:42 +0200

    single import for all the models

---
+++ lib/models/models.dart
@@ -0,0 +1,4 @@
+export 'package:glass_ui/models/absences.dart';
+export 'package:glass_ui/models/grade.dart';
+export 'package:glass_ui/models/student.dart';
+export 'package:glass_ui/models/timetable.dart';
