3f9d9037ff3fc7b717d4726683a4a73ffaf2aa26
Levente <slevi1001@gmail.com>
2023-04-07 19:10:18 +0200

    format

--- lib/bindings/initail_binding.dart
+++ lib/bindings/initail_binding.dart
@@ -1,18 +1,18 @@
 import 'package:get/get.dart';
 // controllers
-import 'package:glass_ui/controllers/api_controller.dart';
+import 'package:glass_ui/services/api_service.dart';
 import 'package:glass_ui/controllers/chart_controller.dart';
 import 'package:glass_ui/controllers/frame_controller.dart';
 import 'package:glass_ui/controllers/login_controller.dart';
-import 'package:glass_ui/controllers/network_controller.dart';
+import 'package:glass_ui/services/network_service.dart';
 
 class InitialBinding implements Bindings {
   @override
   void dependencies() {
-    Get.create(() => LoginController());
-    
-    Get.lazyPut(() => ApiController());
     Get.put<NetworkService>(NetworkService(), permanent: true);
+    
+    Get.create(() => LoginController());
+    Get.lazyPut(() => ApiController());
 
     Get.lazyPut(() => ChartController());
     Get.lazyPut(() => FrameController());
