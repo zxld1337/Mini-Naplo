8c9067940eef84cb00246c1a0cf77a94da1b9370
Levente <slevi1001@gmail.com>
2023-04-11 23:48:26 +0200

    formated code layout

--- lib/bindings/initail_binding.dart
+++ lib/bindings/initail_binding.dart
@@ -1,18 +1,19 @@
 import 'package:get/get.dart';
 // controllers
-import 'package:mini_naplo/services/controllers/api_service.dart';
 import 'package:mini_naplo/services/controllers/chart_controller.dart';
 import 'package:mini_naplo/services/controllers/frame_controller.dart';
 import 'package:mini_naplo/services/controllers/login_controller.dart';
+// services
+import 'package:mini_naplo/services/controllers/api_service.dart';
 import 'package:mini_naplo/services/controllers/network_service.dart';
 
 class InitialBinding implements Bindings {
   @override
   void dependencies() {
     Get.put<NetworkService>(NetworkService());
-    
-    Get.create<LoginController>(() => LoginController());
     Get.lazyPut<ApiService>(() => ApiService());
+        
+    Get.create<LoginController>(() => LoginController());
 
     Get.put<ChartController>(ChartController());
     Get.lazyPut<FrameController>(() => FrameController());
