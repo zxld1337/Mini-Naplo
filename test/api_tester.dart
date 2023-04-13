942e64483cb3fc05f8ff84f63672a510813cb50c
Levente <slevi1001@gmail.com>
2023-04-13 22:50:11 +0200

    renamed test file

--- test/api_tester.dart
+++ test/api_tester.dart
@@ -1,13 +1,12 @@
 // ignore_for_file: avoid_print
 import 'package:mini_naplo/services/client/user.dart';
-import 'package:mini_naplo/1unUsed/secrets.dart';
 //import 'kreta_api.dart';
 
 Future<void> main(List<String> args) async {
   final user = User(
-    user: username,
-    password: password,
-    institute: institute,
+    //user: username,
+    //password: password,
+    //institute: institute,
   );
   await user.init();
   final bool succ = await user.login();
